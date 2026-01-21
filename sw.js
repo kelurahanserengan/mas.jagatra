01eff957a08bfc8240ada96ad2be759e778055dd
kelurahanserengan <kelurahanserenganku@gmail.com>
2026-01-21 15:42:07 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
+const CACHE_NAME = 'masjagatra-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',        // ganti kalau nama CSS berbeda
+  '/img/logo-back-192.png',
+  '/img/logo-back-512.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
 self.addEventListener('fetch', e => {
- e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))
+  e.respondWith(
+    fetch(e.request)
+      .then(res => res)
+      .catch(() => caches.match(e.request))
+  );
 });
